06af264974232aee02095df3f338bc79f7e593d4
Jeevanandh Vijayan <53204857+Jeevanandh007@users.noreply.github.com>
2024-01-29 22:08:47 +0530

    code push

--- code.py
+++ code.py
@@ -1,1 +1,15 @@
+def f4(a,b,n):
+  s=0
+  i=1
+  while i<n:
+    if(i%a)==0 or (i%b)==0:
+      s=s+i
+    i=i+1
+  return s
+
+n=int(input("Enter n ")) 
+a=int(input("Enter a "))
+b=int(input("Enter b "))
+s=f4(a,b,n)
+print(s)
 
